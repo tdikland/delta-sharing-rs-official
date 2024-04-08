407e51b8fbd91239288b234bafa9358a96f7aab4
Tim Dikland <tim.dikland@databricks.com>
2024-04-08 21:25:10 +0200

    extend test cases

--- tests/catalog.rs
+++ tests/catalog.rs
@@ -4,40 +4,55 @@ use delta_sharing::{
     auth::RecipientId,
     catalog::{
         file::{FileCatalog, FileCatalogConfig, FileFormat},
-        Catalog, Pagination,
+        Catalog, CatalogErrorKind, Pagination,
     },
 };
 
 #[tokio::test]
 async fn yaml_file_catalog() {
-    let cfg = FileCatalogConfig::new("./tests/resources/yaml_catalog_file.yaml");
+    let cfg = FileCatalogConfig::new("./tests/fixtures/yaml_catalog_file.yaml");
     let catalog = FileCatalog::new(cfg);
 
     list_shares(&catalog).await;
     list_shares_pagination(&catalog).await;
     list_shares_acl(&catalog).await;
+    list_schemas(&catalog).await;
+    list_tables_in_share(&catalog).await;
+    list_tables_in_schema(&catalog).await;
+    get_share(&catalog).await;
+    get_table(&catalog).await;
 }
 
 #[tokio::test]
 async fn json_file_catalog() {
-    let cfg = FileCatalogConfig::new("./tests/resources/json_catalog_file.json")
+    let cfg = FileCatalogConfig::new("./tests/fixtures/json_catalog_file.json")
         .with_format(FileFormat::Json);
     let catalog = FileCatalog::new(cfg);
 
     list_shares(&catalog).await;
     list_shares_pagination(&catalog).await;
     list_shares_acl(&catalog).await;
+    list_schemas(&catalog).await;
+    list_tables_in_share(&catalog).await;
+    list_tables_in_schema(&catalog).await;
+    get_share(&catalog).await;
+    get_table(&catalog).await;
 }
 
 #[tokio::test]
 async fn toml_file_catalog() {
-    let cfg = FileCatalogConfig::new("./tests/resources/toml_catalog_file.toml")
+    let cfg = FileCatalogConfig::new("./tests/fixtures/toml_catalog_file.toml")
         .with_format(FileFormat::Toml);
     let catalog = FileCatalog::new(cfg);
 
     list_shares(&catalog).await;
     list_shares_pagination(&catalog).await;
     list_shares_acl(&catalog).await;
+    list_schemas(&catalog).await;
+    list_tables_in_share(&catalog).await;
+    list_tables_in_schema(&catalog).await;
+    get_share(&catalog).await;
+    get_table(&catalog).await;
 }
 
 async fn list_shares<C: Catalog>(catalog: &C) {
@@ -102,5 +117,106 @@ async fn list_shares_acl<C: Catalog>(catalog: &C) {
             "public_share_3",
             "private_share_1"
         ])
+    );
+}
+
+async fn list_schemas<C: Catalog>(catalog: &C) {
+    let recipient = RecipientId::anonymous();
+    let pagination = Pagination::default();
+
+    let schemas = catalog
+        .list_schemas("public_share_1", &recipient, &pagination)
+        .await
+        .unwrap();
+    let schema_names = schemas
+        .items()
+        .into_iter()
+        .map(|s| s.name())
+        .collect::<HashSet<_>>();
+    assert_eq!(schema_names, HashSet::from_iter(["schema1", "schema2"]));
+
+    let page = catalog
+        .list_schemas("not-existing-share", &recipient, &pagination)
+        .await
+        .unwrap();
+    assert!(page.is_empty());
+}
+
+async fn list_tables_in_share<C: Catalog>(catalog: &C) {
+    let recipient = RecipientId::anonymous();
+    let pagination = Pagination::default();
+
+    let tables = catalog
+        .list_tables_in_share("public_share_1", &recipient, &pagination)
+        .await
+        .unwrap();
+    let table_names = tables
+        .items()
+        .into_iter()
+        .map(|s| s.name())
+        .collect::<HashSet<_>>();
+    assert_eq!(
+        table_names,
+        HashSet::from_iter(["table1", "table2", "table3"])
+    );
+}
+
+async fn list_tables_in_schema<C: Catalog>(catalog: &C) {
+    let recipient = RecipientId::anonymous();
+    let pagination = Pagination::default();
+
+    let tables = catalog
+        .list_tables_in_schema("public_share_1", "schema1", &recipient, &pagination)
+        .await
+        .unwrap();
+    let table_names = tables
+        .items()
+        .into_iter()
+        .map(|s| s.name())
+        .collect::<HashSet<_>>();
+    assert_eq!(table_names, HashSet::from_iter(["table1", "table2"]));
+}
+
+async fn get_share<C: Catalog>(catalog: &C) {
+    let recipient = RecipientId::anonymous();
+
+    let share = catalog
+        .get_share("public_share_1", &recipient)
+        .await
+        .unwrap();
+    assert_eq!(share.name(), "public_share_1");
+    assert_eq!(share.get_extension("?"), None);
+
+    let recipient = RecipientId::known("unauthorized-client");
+    let share = catalog.get_share("private_share_1", &recipient).await;
+    assert!(share.is_err());
+
+    let recipient = RecipientId::known("recipient1");
+    let share = catalog.get_share("private_share_1", &recipient).await;
+    assert!(share.is_ok())
+}
+
+async fn get_table<C: Catalog>(catalog: &C) {
+    let recipient = RecipientId::anonymous();
+    let table = catalog
+        .get_table("public_share_1", "schema1", "table1", &recipient)
+        .await
+        .unwrap();
+    assert_eq!(table.share_name(), "public_share_1");
+    assert_eq!(table.schema_name(), "schema1");
+    assert_eq!(table.name(), "table1");
+    assert_eq!(
+        table.storage_path(),
+        "s3a://<bucket-name>/<table-prefix-1>/"
+    );
+    assert_eq!(table.get_extension("?"), None);
+
+    let table = catalog
+        .get_table("public_share_1", "schema1", "does-not-exist", &recipient)
+        .await;
+    assert!(table.is_err());
+    assert_eq!(
+        table.unwrap_err().kind(),
+        CatalogErrorKind::ResourceNotFound
     );
 }
